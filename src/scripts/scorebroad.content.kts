f6ae6f046e8fa7e4a7a461d16387f4532f924529
way-zer <himc.wicp@gmail.com>
2020-03-25 00:10:21 +0800

    update scorebroad.content.kts for better color add helpfulCommand.content.kts with /showColor

--- src/scripts/scorebroad.content.kts
+++ src/scripts/scorebroad.content.kts
@@ -8,10 +8,10 @@ import java.util.*
 name.set("扩展功能: 积分榜")
 //建议只修改下面一段,其他地方代码请勿乱动
 val msg = """
-    [green]欢迎{player.name}[green]来到WZ服务器
-    [blue]当前地图为: [yellow]{map.name}
-    [blue]服务器FPS: [yellow]{fps}
-    [green]输入/broad可以开关该显示
+    [magenta]欢迎[goldenrod]{player.name}[magenta]来到WZ服务器
+    [violet]当前地图为: [orange]{map.name}
+    [violet]服务器FPS: [orange]{fps}
+    [royal]输入/broad可以开关该显示
 """.trimIndent()
 
 val disabled = mutableSetOf<String>()
@@ -20,9 +20,9 @@ registerScheduleTask("loop", true, ScheduleTask<Unit> {
     playerGroup.forEach {
         if(disabled.contains(it.uuid))return@forEach
         if (it.isMobile) {
-            Call.onInfoPopup(it.con, msg.with("player" to it).toString(), 2.013f, Align.topLeft, 150, 0, 0, 0)
+            Call.onInfoPopup(it.con, msg.with("player" to it).toString(), 2.013f, Align.topLeft, 210, 0, 0, 0)
         } else
-            Call.onInfoPopup(it.con, msg.with("player" to it).toString(), 2.013f, Align.topLeft, 110, 0, 0, 0)
+            Call.onInfoPopup(it.con, msg.with("player" to it).toString(), 2.013f, Align.topLeft, 155, 0, 0, 0)
     }
     return@ScheduleTask Duration.ofSeconds(2).delayToDate()
 })
