7464e7a7fe430dfad3eee2f04bef9cd25d653d07
way-zer <himc.wicp@gmail.com>
2020-03-25 15:46:40 +0800

    update plugin to v1.0.2 update variables.content.kts for new PlaceHoldApi

--- src/scripts/variables.content.kts
+++ src/scripts/variables.content.kts
@@ -1,69 +1,48 @@
 //WayZer 版权所有(请勿删除版权注解)
+package scripts
+
 import arc.util.Time
-import cf.wayzer.placehold.DynamicResolver
 import cf.wayzer.placehold.DynamicVar
-import cf.wayzer.placehold.PlaceHoldContext
-import cf.wayzer.placehold.types.DateResolver
 import mindustry.entities.type.Player
 import mindustry.game.Team
 import mindustry.maps.Map
-import java.util.*
 
 name.set("基础: 全局变量")
 
-class MapVars(private val map:Map) : DynamicVar<Map>(){
-    override fun resolveThis(context: PlaceHoldContext): Map? {
-        return map
-    }
-    init {
-        registerChild("name"){it?.name()}
-//        registerChild("id"){it?.name()}
-        registerChild("desc"){it?.description()}
-        registerChild("author"){it?.author()}
-        registerChild("width"){it?.width}
-        registerChild("height"){it?.height}
-        registerChild("fileName"){it?.file?.nameWithoutExtension()}
-    }
-}
-class PlayerVars(private val player:Player): DynamicVar<Player>(){
-    override fun resolveThis(context: PlaceHoldContext): Player? {
-        return player
-    }
-    init {
-        registerChild("name"){it?.name}
-        registerChild("uuid"){it?.uuid}
-        registerChild("ip"){it?.con?.address}
-//        registerChild("_info"){it?.info}
-        registerChild("_team"){it?.team}
-    }
-}
-class TeamVars(private val team:Team): DynamicVar<Team>(){
-    override fun resolveThis(context: PlaceHoldContext): Team? {
-        return team
-    }
-    init {
-        registerChild("name"){it?.name}
-        registerChild("color"){it?.let { "[#${it.color}]" }}
-        registerChild("colorizeName") {  resolveChild(this,"color")?.toString()+ resolveChild(this,"name")?.toString() }
-    }
-}
-
-registerTypeVar(Date::class.java){DateResolver(it as Date)}
-registerTypeVar(Map::class.java){MapVars(it as Map)}
-registerTypeVar(Player::class.java){PlayerVars(it as Player)}
-registerTypeVar(Team::class.java){TeamVars(it as Team)}
 //SystemVars
-registerVar("fps",DynamicResolver.new {
+PlaceHoldApi.registerGlobalDynamicVar("fps") { _, _ ->
     (60f / Time.delta()).toInt()
-})
-registerVar("heapUse",DynamicResolver.new {
+}
+PlaceHoldApi.registerGlobalDynamicVar("heapUse") { _, _ ->
     Core.app.javaHeap / 1024 / 1024  //MB
-})
+}
 
 //GameVars
-registerVar("map",DynamicResolver.new { world.map })
-registerVar("state.allUnit",DynamicResolver.new { unitGroup.size() })
-registerVar("state.allBan",DynamicResolver.new { netServer.admins.banned.size })
-registerVar("state.playerSize",DynamicResolver.new { playerGroup.size() })
+PlaceHoldApi.registerGlobalDynamicVar("map") { _, _ -> world.map }
+PlaceHoldApi.typeBinder<Map>().apply {
+    registerChild("name", DynamicVar { it, _ -> it.name() })
+//        registerChild("id",DynamicVar{it,_->it.name()})
+    registerChild("desc", DynamicVar { it, _ -> it.description() })
+    registerChild("author", DynamicVar { it, _ -> it.author() })
+    registerChild("width", DynamicVar { it, _ -> it.width })
+    registerChild("height", DynamicVar { it, _ -> it.height })
+    registerChild("fileName", DynamicVar { it, _ -> it.file?.nameWithoutExtension() })
+}
+PlaceHoldApi.registerGlobalDynamicVar("state.allUnit") { _, _ -> unitGroup.size() })
+PlaceHoldApi.registerGlobalDynamicVar("state.allBan") { _, _ -> netServer.admins.banned.size })
+PlaceHoldApi.registerGlobalDynamicVar("state.playerSize") { _, _ -> playerGroup.size() })
+
 //PlayerVars
-registerVar("team",DynamicResolver.new { getVar("player._team") })
+PlaceHoldApi.typeBinder<Player>().apply {
+    registerChild("name", DynamicVar { it, _ -> it.name })
+    registerChild("uuid", DynamicVar { it, _ -> it.uuid })
+    registerChild("ip", DynamicVar { it, _ -> it.con?.address })
+//        registerChild("_info",DynamicVar{it,_->it.info})
+    registerChild("team", DynamicVar { it, _ -> it.team })
+}
+PlaceHoldApi.registerGlobalDynamicVar("team") { _, _ -> getVar("player.team") })
+PlaceHoldApi.typeBinder<Team>().apply {
+    registerChild("name", DynamicVar { it, _ -> it.name })
+    registerChild("color", DynamicVar { it, _ -> it.let { "[#${it.color}]" } })
+    registerChild("colorizeName", DynamicVar { it, _ -> typeResolve(it, "color")?.toString() + typeResolve(it, "name")?.toString() })
+}
