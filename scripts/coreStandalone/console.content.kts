3641bfd3bdb864364c8642be5825cf5ca82d00f1
way-zer <himc.wicp@gmail.com>
2020-05-23 20:47:15 +0800

    (coreStandalone) update console.content.kts

--- scripts/coreStandalone/console.content.kts
+++ scripts/coreStandalone/console.content.kts
@@ -2,8 +2,15 @@ import kotlin.concurrent.thread
 
 onEnable {
     thread(true, isDaemon = true) {
-        val console = System.console() ?: return@thread
         val consoleSender = Sender()
+        val console = System.console() ?: let {
+            println("No console,use System.in")
+            return@thread System.`in`.reader().useLines { lines ->
+                lines.forEach {
+                    RootCommands.handle(consoleSender, it)
+                }
+            }
+        }
         while (enabled) {
             val line = console.readLine("> ")
             RootCommands.handle(consoleSender, line)
