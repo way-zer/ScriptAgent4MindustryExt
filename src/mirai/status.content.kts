2346087400a69bf8a2ef0e36aa93a6056e676937
way-zer <himc.wicp@gmail.com>
2020-05-27 13:48:41 +0800

    [bug](mirai/status) fix const value

--- src/mirai/status.content.kts
+++ src/mirai/status.content.kts
@@ -1,11 +1,11 @@
 package mirai
 
 subscribeGroupMessages {
-    case("服务器状态").quoteReply(
-            """
-                当前地图为: {map.name} 波数: {state.wave}
-                服务器FPS: {fps} 内存占用(MB) {heapUse}
-                当前人数: {state.playerSize} 总单位数: {state.allUnit}
-            """.trimIndent().with().toString()
-    )
+    case("服务器状态").quoteReply {
+        """
+            当前地图为: {map.name} 波数: {state.wave}
+            服务器FPS: {fps} 内存占用(MB) {heapUse}
+            当前人数: {state.playerSize} 总单位数: {state.allUnit}
+        """.trimIndent().with().toString()
+    }
 }
