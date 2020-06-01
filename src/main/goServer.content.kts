74743609a21b4d80bd54a293a685a39d0456aaac
way-zer <himc.wicp@gmail.com>
2020-06-01 23:08:34 +0800

    [Added](main) add goServer.content.kts | 跨服传送

---
+++ src/main/goServer.content.kts
@@ -0,0 +1,27 @@
+package main
+//WayZer 版权所有(请勿删除版权注解)
+import mindustry.gen.Call
+
+name = "跨服传送"
+
+val servers by config.key(mapOf<String, String>(), "服务器传送列表", "格式: {名字: \"介绍;地址\"} (;作为分割符)")
+
+data class Info(val name: String, val desc: String, val address: String, val port: Int)
+
+val infos: Map<String, Info>
+    get() = servers.mapValues { (k, v) ->
+        val sp1 = v.split(";")
+        assert(sp1.size == 2) { "格式错误: $v" }
+        val sp2 = sp1[1].split(":")
+        val port = sp2.getOrNull(1)?.toIntOrNull() ?: port
+        Info(k, sp1[0], sp2[0], port)
+    }
+
+command("go", "传送到其他服务器", "[名字,为空列出]", CommandType.Client) { arg, p ->
+    val info = arg.firstOrNull()?.let { infos[it] ?: return@command p.sendMessage("[red]错误的服务器名字".with()) } ?: let {
+        val list = infos.values.map { "[gold]{name}:[tan]{desc}\n".with("name" to it.name, "desc" to it.desc) }
+        return@command p.sendMessage("[violet]可用服务器: \n{list}".with("list" to list))
+    }
+    Call.onConnect(p!!.con, info.address, info.port)
+    broadcast("[cyan][-][salmon]{player.name}[salmon]传送到了{name}服务器".with("player" to p, "name" to info.name))
+}
