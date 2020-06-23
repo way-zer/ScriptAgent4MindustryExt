7b075e94c69c7de698ca590521203b25e84e20bc
way-zer <himc.wicp@gmail.com>
2020-06-23 23:18:58 +0800

    [Fixed](wayzer/ext/serverStatus)颜色被转义bug

--- src/wayzer/ext/serverStatus.content.kts
+++ src/wayzer/ext/serverStatus.content.kts
@@ -2,7 +2,7 @@ package wayzer.ext
 
 val format by config.key("""
     |[green]服务器状态[]
-    |   [green]地图: [[yellow]{map.id}[green]][yellow]{map.name}[green] 模式: [yellow]{map.mode}
+    |   [green]地图: [ [yellow]{map.id} [green]][yellow]{map.name}[green] 模式: [yellow]{map.mode}
     |   [green]{fps} FPS, {heapUse} MB used[]
     |   [green]总单位数: {state.allUnit} 玩家数: {state.playerSize}
     |   [yellow]被禁封总数: {state.allBan}
