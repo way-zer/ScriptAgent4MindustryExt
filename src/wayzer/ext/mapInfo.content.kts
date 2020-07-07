e257f06305106d803b047b88103c92fa79ad3330
way-zer <himc.wicp@gmail.com>
2020-07-07 11:52:48 +0800

    [Update](wayzer/ext/mapInfo) 修改显示时间为5分钟

--- src/wayzer/ext/mapInfo.content.kts
+++ src/wayzer/ext/mapInfo.content.kts
@@ -9,7 +9,7 @@ import mindustry.world.blocks.storage.CoreBlock
 fun CoreBlock.CoreEntity.showInfo(p:Player){
     if(p.con==null)return
     fun draw(dotY:Float,text:String)
-        = Call.onLabel(p.con,text, 1.0E9F,x,y+dotY)
+        = Call.onLabel(p.con,text, 60*60*5f/*5分钟*/,x,y+dotY)
     draw(0f,"[purple]====[white]${world.map.name()}[purple]====")
     draw(-10f,"[purple]By: [scarlet]${world.map.author()}")
     world.map.description().apply {
