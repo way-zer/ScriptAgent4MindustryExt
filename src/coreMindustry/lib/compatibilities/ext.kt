15b61d628fcadb3d00b48f1d9a9115cb1d36a338
way-zer <himc.wicp@gmail.com>
2020-08-24 16:35:20 +0800

    [Update](coreMindustry) 更新兼容层

--- src/coreMindustry/lib/compatibilities/ext.kt
+++ src/coreMindustry/lib/compatibilities/ext.kt
@@ -2,10 +2,13 @@ package coreMindustry.lib.compatibilities
 
 import mindustry.Vars
 import mindustry.core.World
+import mindustry.game.Team
 import mindustry.gen.Player
 import mindustry.maps.Map
 
 inline val Player.isAdmin get() = admin
 inline val Player.isMobile get() = con?.mobile ?: false
+inline val Player.uuid: String get() = uuid()
+inline val Player.team: Team get() = team()
 
 inline val World.map: Map get() = Vars.state.map
