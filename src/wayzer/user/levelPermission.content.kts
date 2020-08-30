539bd1ae3a00505d0898d6142c77ea4cf42ab209
way-zer <himc.wicp@gmail.com>
2020-08-30 20:32:36 +0800

    [Update](wayzer/user/skills&levelPermission) 添加 灭火技能

--- src/wayzer/user/levelPermission.content.kts
+++ src/wayzer/user/levelPermission.content.kts
@@ -6,8 +6,9 @@ import coreLibrary.lib.event.PermissionRequestEvent
 val PlayerProfile.level by PlaceHold.referenceForType<Int>("level")
 listenTo<PermissionRequestEvent> {
     val profile = context.player?.let { PlayerData.getOrNull(it.uuid) }?.profile ?: return@listenTo
-    when (permission) {
-        "wayzer.user.skills.draug" -> profile.level >= 2
+    result = when (permission) {
+        "wayzer.user.skills.draug" -> profile.level >= 1
+        "wayzer.user.skills.noFire" -> profile.level >= 2
+        else -> return@listenTo
     }
 }
-state
--- src/wayzer/user/skills.content.kts
+++ src/wayzer/user/skills.content.kts
@@ -1,12 +1,18 @@
 package wayzer.user
 
+import mindustry.content.Blocks
 import mindustry.content.UnitTypes
 import mindustry.entities.type.Player
 import mindustry.game.EventType
 import mindustry.game.Gamemode
+import java.time.Duration
 
-val used by PlaceHold.reference<MutableSet<String>>("_skills_used_set")
-registerVar("_skills_used_set", "内部使用:技能系统已使用集合", runCatching { used }.getOrElse { mutableSetOf() })
+val used by PlaceHold.reference<MutableMap<String, Long>>("_skills_used_set")
+registerVar("_skills_used_set", "内部使用:技能系统已使用集合", runCatching { used }.getOrElse { mutableMapOf() })
+
+listen<EventType.ResetEvent> {
+    used.clear()
+}
 
 fun skill(name: String, desc: String, vararg aliases: String, body: CommandContext.() -> Unit) {
     command(name, desc, {
@@ -16,7 +22,10 @@ fun skill(name: String, desc: String, vararg aliases: String, body: CommandConte
     }, body)
 }
 
-fun CommandContext.checkBefore(allowPvp: Boolean, useId: String?) {
+/**
+ * @param coolDown in ms; <0 ->一局冷却
+ */
+fun CommandContext.checkBefore(allowPvp: Boolean, coolDown: Int?) {
     if (state.rules.mode() !in arrayOf(Gamemode.survival, Gamemode.attack) && (!allowPvp || state.rules.pvp)) {
         reply("[red]当前模式禁用".with())
         CommandInfo.Return()
@@ -25,29 +34,40 @@ fun CommandContext.checkBefore(allowPvp: Boolean, useId: String?) {
         reply("[red]你已死亡".with())
         CommandInfo.Return()
     }
-    if (useId != null) {
+    if (coolDown != null) {
         val id = PlayerData[player!!.uuid].profile?.id?.value ?: 0
-        val key = "${useId}@$id"
+        val key = "${thisCommand.name}@$id"
         if (key in used) {
-            reply("[red]你已使用该技能".with())
-            CommandInfo.Return()
-        } else used.add(key)
+            if (coolDown < 0) {
+                reply("[red]该技能每局限用一次".with())
+                CommandInfo.Return()
+            } else if (used[key]!! >= System.currentTimeMillis()) {
+                reply("[red]技能冷却，还剩{time:秒}".with("time" to Duration.ofMillis(used[key]!! - System.currentTimeMillis())))
+                CommandInfo.Return()
+            }
+        }
+        used[key] = System.currentTimeMillis() + coolDown
     }
 }
 
 fun Player.broadcastSkill(skill: String) = broadcast("[yellow][技能][green]{player.name}[white]使用了[green]{skill}[white]技能.".with("player" to this, "skill" to skill), quite = true)
 
 skill("draug", "技能: 召唤采矿机,一局限一次,PVP禁用", "矿机") {
-    checkBefore(false, "矿机")
-    repeat(2) {
-        UnitTypes.draug.create(player!!.team).apply {
-            set(player!!.x, player!!.y)
-            add()
-        }
+    checkBefore(false, -1)
+    if (state.rules.bannedBlocks.contains(Blocks.draugFactory))
+        return@skill reply("[red]该地图采矿机已禁封,禁止召唤".with())
+    UnitTypes.draug.create(player!!.team).apply {
+        set(player!!.x, player!!.y)
+        add()
     }
     player!!.broadcastSkill("召唤采矿机")
 }
 
-listen<EventType.ResetEvent> {
-    used.clear()
+skill("noFire", "技能: 灭火,半径10格,冷却120s", "灭火") {
+    checkBefore(false, 120_000)
+    fireGroup.forEach {
+        if (it.dst(player!!) <= 80)
+            it.remove()
+    }
+    player!!.broadcastSkill("灭火")
 }
