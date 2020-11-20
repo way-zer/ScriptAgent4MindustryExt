252a8fc2041cee414dfd74c44caa0368e61f97fe
way-zer <himc.wicp@gmail.com>
2020-11-20 21:49:06 +0800

    [Update](mirai/status) 增加服务器地址及版本显示

--- scripts/mirai/status.content.kts
+++ scripts/mirai/status.content.kts
@@ -3,15 +3,19 @@ package mirai
 import cf.wayzer.placehold.PlaceHoldApi
 import java.awt.image.BufferedImage
 
+val address by config.key("", "服务器地址,为空则不显示")
+
 subscribeGroupMessages {
     case("服务器状态") {
         @Suppress("UNCHECKED_CAST")
         val getMapSnap = PlaceHoldApi.GlobalContext.getVar("wayzer.ext.mapSnap._get") as? () -> BufferedImage
+        val addressInfo = if (address.isEmpty()) "" else "服务器地址: $address "
         var msg = this.message.quote() + """
+                {addressInfo}版本 {game.version}
                 当前地图为: {map.name} 波数: {state.wave}
                 服务器FPS: {fps} 内存占用(MB) {heapUse}
                 当前人数: {state.playerSize} 总单位数: {state.allUnit}
-            """.trimIndent().with().toString()
+            """.trimIndent().with("addressInfo" to addressInfo).toString()
         getMapSnap?.let{
             msg += uploadImage(it())
         }
