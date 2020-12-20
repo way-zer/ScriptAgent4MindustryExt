bbe4e79e17a894aa994f63b6324df5b6b36df2f0
way-zer <himc.wicp@gmail.com>
2020-12-20 13:55:06 +0800

    :sparkles: [mirai/codeSender] 重写

--- scripts/mirai/codeSender.content.kts
+++ scripts/mirai/codeSender.content.kts
@@ -1,26 +1,22 @@
 package mirai
 
-subscribeFriendMessages {
+subscribeMessages {
     case("绑定") {
+        if (this is GroupMessageEvent) {
+            reply("绑定请私聊我")
+            return@case
+        }
         val qq = sender.id
         val generate = depends("wayzer/user/profileBind")?.import<(Long) -> Int>("generate")
         if (generate == null) {
             reply("绑定服务暂不可用，请联系管理")
             return@case
         }
-        reply("你好${sender.nick}\n" +
-                "复制'/bind ${generate(qq).toString().padStart(6, '0')}'到游戏中，即可完成绑定。")
-    }
-}
-subscribeTempMessages{
-    case("绑定") {
-        val qq = sender.id
-        val generate = depends("wayzer/user/profileBind")?.import<(Long) -> Int>("generate")
-        if (generate == null) {
-            reply("绑定服务暂不可用，请联系管理")
-            return@case
-        }
-        reply("你好${sender.nameCard}\n" +
-                "复制'/bind ${generate(qq).toString().padStart(6, '0')}'到游戏中，即可完成绑定。")
+        reply(
+            """
+            你好${sender.nick}
+            复制'/bind ${generate(qq).toString().padStart(6, '0')}'到游戏中，即可完成绑定。
+        """.trimIndent()
+        )
     }
 }
