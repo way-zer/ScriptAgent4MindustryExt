e531b9baadf04b1bb3ab9dce8aace24f2e2646fb
way-zer <himc.wicp@gmail.com>
2021-01-09 16:18:16 +0800

    :sparkles: (core)增加 Player.hasPermission

--- scripts/coreMindustry/lib/CommandExt.kt
+++ scripts/coreMindustry/lib/CommandExt.kt
@@ -1,12 +1,21 @@
 package coreMindustry.lib
 
 import cf.wayzer.script_agent.ISubScript
+import coreLibrary.lib.CommandContext
 import coreLibrary.lib.CommandHandler
 import coreLibrary.lib.CommandInfo
+import coreLibrary.lib.event.PermissionRequestEvent
 import mindustry.gen.Player
 
 fun ISubScript.command(name: String, description: String, init: CommandInfo.() -> Unit) {
     RootCommands += CommandInfo(this, name, description, init)
+}
+
+@Suppress("unused")
+fun Player.hasPermission(permission: String): Boolean {
+    return CommandContext().also {
+        it.player = this
+    }.hasPermission(permission)
 }
 
 @Deprecated("use new command api", ReplaceWith("command(name,description){init\nbody(handler)}"))
