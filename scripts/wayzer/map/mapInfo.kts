9ce74c2f1d5f2b1f01300f4ec5a60cf1f129e997
way-zer <himc.wicp@gmail.com>
2021-01-30 22:44:41 +0800

    :bug: (wayzer/map/mapInfo) 修复换图时无法显示的bug

--- scripts/wayzer/map/mapInfo.kts
+++ scripts/wayzer/map/mapInfo.kts
@@ -8,15 +8,16 @@ import mindustry.world.blocks.storage.CoreBlock
 
 fun CoreBlock.CoreBuild.showInfo(p: Player) {
     if (p.con == null) return
-    fun draw(dotY: Float, text: String) = Call.label(p.con, text, 60 * 5f/*5分钟*/, x, y + dotY)
+    fun draw(dotY: Float, text: String) = Call.label(p.con, text, 60f/*1分钟*/, x, y + dotY)
     draw(0f, "[purple]====[white]${state.map.name()}[purple]====")
     draw(-10f, "[purple]By: [scarlet]${state.map.author()}")
     state.map.description().apply {
         var lastEnd = -1
         var pos = -20f
         while (lastEnd < length) {
-            val newEnd = indexOfAny(charArrayOf(' ', '，', ',', '.', '。'), lastEnd + 30).takeUnless { it == -1 }
-                ?: length
+            val newEnd =
+                indexOfAny(charArrayOf(' ', '，', ',', '.', '。', '!', '！'), lastEnd + 30).takeUnless { it == -1 }
+                    ?: length
             draw(pos, substring(lastEnd + 1, newEnd))
             lastEnd = newEnd
             pos += -10f
@@ -25,10 +26,13 @@ fun CoreBlock.CoreBuild.showInfo(p: Player) {
 //    draw(-20f, world.map.description())
 }
 
-listen<EventType.WorldLoadEvent>{
-    Core.app.post{
-        Groups.player.forEach {
-            it.team().cores().firstOrNull()?.showInfo(it)
+listen<EventType.PlayEvent> {
+    launch {
+        delay(5000)
+        launch(Dispatchers.game) {
+            Groups.player.forEach {
+                it.team().cores().firstOrNull()?.showInfo(it)
+            }
         }
     }
 }
