85bc0474163b544be57b2da2e9c1476991bb6b8e
way-zer <himc.wicp@gmail.com>
2021-03-14 17:33:15 +0800

    [Update](coreLib/variables) 使Config支持Duration

--- scripts/coreLibrary/variables.kts
+++ scripts/coreLibrary/variables.kts
@@ -1,5 +1,10 @@
 package coreLibrary
 
+import com.typesafe.config.Config
+import io.github.config4k.ClassContainer
+import io.github.config4k.CustomType
+import io.github.config4k.registerCustomType
+import io.github.config4k.toConfig
 import java.text.SimpleDateFormat
 import java.time.Duration
 import java.time.Instant
@@ -25,3 +30,26 @@ registerVarForType<Duration>().apply {
         "%.2f%s".format((obj.seconds.toDouble() / unit.duration.seconds), arg ?: "")
     }
 }
+
+@Suppress("PropertyName")
+val NANO_PRE_SECOND = 1000_000_000L
+fun Duration.toConfigString(): String {
+    //Select the smallest unit output
+    return when {
+        (nano % 1000) != 0 -> (seconds * NANO_PRE_SECOND + nano).toString() + "ns"
+        (nano % 1000_000) != 0 -> ((seconds * NANO_PRE_SECOND + nano) / 1000).toString() + "us"
+        nano != 0 -> ((seconds * NANO_PRE_SECOND + nano) / 1000_000).toString() + "ms"
+        (seconds % 60) != 0L -> seconds.toString() + "s"
+        (seconds % (60 * 60)) != 0L -> (seconds / 60).toString() + "m"
+        (seconds % (60 * 60 * 24)) != 0L -> (seconds / (60 * 60)).toString() + "h"
+        else -> (seconds / 60).toString() + "d"
+    }
+}
+registerCustomType(object : CustomType {
+    override fun testParse(clazz: ClassContainer) = false
+    override fun parse(clazz: ClassContainer, config: Config, name: String) = UnsupportedOperationException()
+    override fun testToConfig(obj: Any) = obj is Duration
+    override fun toConfig(obj: Any, name: String): Config {
+        return (obj as Duration).toConfigString().toConfig(name)
+    }
+})
