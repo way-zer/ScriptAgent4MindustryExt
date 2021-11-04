e4fa2477e75b0d598b3b15d71635091c1af491e8
way-zer <himc.wicp@gmail.com>
2021-11-05 01:01:14 +0800

    :wrench: (wayzer/user/ban) 暴露ban接口

--- scripts/wayzer/user/ban.kts
+++ scripts/wayzer/user/ban.kts
@@ -21,7 +21,7 @@ fun Player.kick(profile: PlayerProfile, ban: PlayerBan) {
         [green]禁封时间: ${format(ban.createTime)}
         [green]解禁时间: ${format(ban.endTime)}
         [yellow]如有问题,请截图此页咨询管理员
-    """.trimIndent()
+    """.trimIndent(), 0
     )
 }
 
@@ -35,6 +35,25 @@ listen<EventType.PlayerConnect> {
     }
 }
 
+fun ban(uuid: String, time: Int, reason: String, operate: PlayerProfile?): PlayerProfile? {
+    val profile = (PlayerData.findById(uuid) ?: return null).profile
+    if (profile == null) {
+        netServer.admins.banPlayerID(uuid)
+        netServer.admins.getInfoOptional(uuid)?.let {
+            broadcast("[red] 管理员禁封了{target.name},原因: [yellow]{reason}".with("target" to it, "reason" to reason))
+        }
+    } else {
+        val ban = PlayerBan.create(profile, Duration.ofMinutes(time.toLong()), reason, operate)
+        Groups.player.find { it.uuid() == uuid }?.let {
+            it.kick(profile, ban)
+            broadcast("[red] 管理员禁封了{target.name},原因: [yellow]{reason}".with("target" to it, "reason" to reason))
+        }
+    }
+    return profile
+}
+
+export(::ban)
+
 command("banX", "管理指令: 禁封") {
     usage = "<3位id> <时间|分钟> <原因>"
     permission = "wayzer.admin.ban"
@@ -45,20 +64,8 @@ command("banX", "管理指令: 禁封") {
             ?: returnReply("[red]请输入目标3位ID,不清楚可通过/list查询".with())
         val time = arg[1].toIntOrNull()?.takeIf { it > 0 } ?: replyUsage()
         val reason = arg.slice(2 until arg.size).joinToString(" ")
-        val profile = PlayerData[uuid].profile
         val operate = player?.let { PlayerData[it.uuid()].profile }
-        if (profile == null) {
-            netServer.admins.banPlayerID(uuid)
-            netServer.admins.getInfoOptional(uuid)?.let {
-                broadcast("[red] 管理员禁封了{target.name},原因: [yellow]{reason}".with("target" to it, "reason" to reason))
-            }
-        } else {
-            val ban = PlayerBan.create(profile, Duration.ofMinutes(time.toLong()), reason, operate)
-            Groups.player.find { it.uuid() == uuid }?.let {
-                it.kick(profile, ban)
-                broadcast("[red] 管理员禁封了{target.name},原因: [yellow]{reason}".with("target" to it, "reason" to reason))
-            }
-            reply("[green]已禁封{qq}".with("qq" to profile.qq))
-        }
+        val qq = ban(uuid, time, reason, operate)?.qq
+        reply("[green]已禁封{qq}".with("qq" to (qq ?: uuid)))
     }
 }
