2e89805acc185a324e531c92ab42490be3b1217e
way-zer <himc.wicp@gmail.com>
2021-11-05 00:32:58 +0800

    :sparkles: (mirai/status) 增加更多内容

--- scripts/mirai/status.kts
+++ scripts/mirai/status.kts
@@ -16,7 +16,8 @@ globalEventChannel().subscribeGroupMessages {
         val addressInfo = if (address.isEmpty()) "" else "服务器地址: $address "
         var msg = this.message.quote() + """
                 {addressInfo}版本 {game.version}
-                当前地图为: {map.name} 波数: {state.wave}
+                当前地图为: [{map.id}]{map.name} 
+                波数: {state.wave} 本局游戏时间: {state.gameTime:分钟}
                 服务器FPS: {fps} 内存占用(MB) {heapUse}
                 当前人数: {state.playerSize} 总单位数: {state.allUnit}
             """.trimIndent().with("addressInfo" to addressInfo, "receiver" to this.sender).toString()
