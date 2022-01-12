a589f5c8cb25ff9b28b2869ef112916a8357092f
way-zer <himc.wicp@gmail.com>
2022-01-12 15:55:20 +0800

    :arrow_up: (coreLib/DBApi) 升级 Exposed 到 0.37.3

--- DBApi.kts
+++ DBApi.kts
@@ -1,7 +1,7 @@
-@file:Import("org.jetbrains.exposed:exposed-core:0.35.1", mavenDepends = true)
-@file:Import("org.jetbrains.exposed:exposed-dao:0.35.1", mavenDepends = true)
-@file:Import("org.jetbrains.exposed:exposed-java-time:0.35.1", mavenDepends = true)
-@file:Import("org.jetbrains.exposed:exposed-jdbc:0.35.1", mavenDepends = true)
+@file:Import("org.jetbrains.exposed:exposed-core:0.37.3", mavenDepends = true)
+@file:Import("org.jetbrains.exposed:exposed-dao:0.37.3", mavenDepends = true)
+@file:Import("org.jetbrains.exposed:exposed-java-time:0.37.3", mavenDepends = true)
+@file:Import("org.jetbrains.exposed:exposed-jdbc:0.37.3", mavenDepends = true)
 
 package coreLibrary
 
