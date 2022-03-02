64715311bd068124a0f529cd41aaa74c0980ba4e
way-zer <himc.wicp@gmail.com>
2022-03-02 11:58:44 +0800

    :sparkles: (core/utilContentOverwrite) 更新到1.0.6,支持配置项修改资源包版本号

--- utilMapRule.kts
+++ utilMapRule.kts
@@ -1,8 +1,34 @@
 package coreMindustry
 
+import mindustry.core.ContentLoader
+import mindustry.ctype.Content
+import mindustry.ctype.MappableContent
+import java.lang.reflect.Modifier
 import kotlin.reflect.KMutableProperty0
 
 val bakMap = mutableMapOf<KMutableProperty0<*>, Any?>()
+
+/**Should invoke in [Dispatchers.game] */
+fun <T : Content, R : T> newContent(origin: T, block: (origin: T) -> R): R {
+    val bak = content
+    content = object : ContentLoader() {
+        override fun transformName(name: String?) = bak?.transformName(name) ?: name
+        override fun handleContent(content: Content?) = Unit
+        override fun handleMappableContent(content: MappableContent?) = Unit
+    }
+    return try {
+        block(origin).also { new ->
+            origin::class.java.fields.forEach {
+                if (!it.declaringClass.isInstance(new)) return@forEach
+                if (Modifier.isPublic(it.modifiers) && !Modifier.isFinal(it.modifiers)) {
+                    it.set(new, it.get(origin))
+                }
+            }
+        }
+    } finally {
+        content = bak
+    }
+}
 
 fun <T> registerMapRule(field: KMutableProperty0<T>, valueFactory: (T) -> T) {
     synchronized(bakMap) {
