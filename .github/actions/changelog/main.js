c5180c416cad06204a255d4a1641e8ad657580c3
way-zer <himc.wicp@gmail.com>
2022-03-16 17:58:11 +0800

    :memo: (ci) 更新changelog功能

--- .github/actions/changelog/main.js
+++ .github/actions/changelog/main.js
@@ -7,10 +7,11 @@ let octokit = getOctokit(token)
 let lastRelease = (await octokit.rest.repos.getLatestRelease(context.repo)).data.tag_name
 core.info("Find last release: " + lastRelease)
 
-let commits = (await octokit.rest.repos.listCommits({
-    ...context.repo, since: lastRelease
+let compare = (await octokit.rest.repos.compareCommits({
+    ...context.repo, base: lastRelease, head: context.sha
 })).data
-let changes = commits.map(commit => {
+
+let changes = compare.commits.map(commit => {
     let {sha, author, commit: {message}} = commit
     let [title, ...body] = message.split("\n")
 
@@ -21,9 +22,27 @@ let changes = commits.map(commit => {
 }).join("\n")
 core.setOutput("changes", changes)
 
+let changeFiles = compare.files.map(file => {
+    switch (file.status) {
+        case 'modified':
+            return `* :memo: ${file.filename} +${file.additions} -${file.deletions}`
+        case 'added':
+            return `* :heavy_plus_sign: ${file.filename}`
+        case "removed":
+            return `* :fire: ${file.filename}`
+        case "renamed":
+            return `* :truck: ${file.filename} <= ${file.previous_filename}`
+        default:
+            return `* ${file.status} ${file.filename}`
+    }
+})
+
 core.setOutput("releaseBody", `
-# 预发布版本，仅供测试使用
+# 预发布版本，仅供测试使用;正式发布前，可能会多次更新(以build号为准)
 ---
 ## 更新日记
 ${changes}
+## 文件变更
+${changeFiles}
+[完整对比](${compare.html_url}) [完整patch](${compare.patch_url})
 `)
