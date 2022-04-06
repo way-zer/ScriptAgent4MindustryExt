caddc42bb40feafa89a6aa6dc7159e944561e978
way-zer <himc.wicp@gmail.com>
2022-04-06 13:51:51 +0800

    :heavy_plus_sign: (mapScript/13522) 科技战争

--- scripts/mapScript/13522.kts
+++ scripts/mapScript/13522.kts
@@ -2,32 +2,39 @@ package mapScript
 
 import arc.util.Align
 import coreLibrary.lib.util.loop
-import mindustry.game.Team
-/**@author Lucky_Clover
- * ClearUp: WayZer */
-onEnable {
-    val initState = Team.all.associateWith { it.rules().buildSpeedMultiplier }
-    val totalCore = state.teams.getActive().sum { it.cores.size }
+import mindustry.gen.Groups
 
-    val techSpeed = 0.002f
-    val techAverage = 1 + techSpeed * totalCore / state.teams.active.count()
+/**@author Lucky_Clover WayZer */
+name = "科技战争"
+onEnable {
+    val techSpeed = state.rules.tags.getFloat("@techSpeed", 0.002f)
 
     loop(Dispatchers.game) {
         delay(5000)
+
+        val teams = Groups.player.mapTo(mutableSetOf()) { it.team() }
+        teams.removeAll { !it.active() }
+        val averageCore = teams.sumOf { it.cores().size }.toFloat() / teams.size
+
         var infotext = "[red]科[yellow]技[green]战[blue]争[white]。\n[violet]规则[white]：核心数量越多科技越快，提高队伍全局属性\n"
-        infotext += state.teams.getActive().joinToString("\n") { team ->
-            val teamTechSpeed = (1 + techSpeed * team.cores.size)
-            team.team.rules().blockHealthMultiplier *= teamTechSpeed / techAverage
-            team.team.rules().blockDamageMultiplier *= teamTechSpeed / techAverage
-            team.team.rules().unitDamageMultiplier *= teamTechSpeed / techAverage
+        infotext += teams.joinToString("\n") { team ->
+            val teamTechSpeed = 1 + techSpeed * (team.cores().size / averageCore - 1)
+            val delta = team.rules().blockHealthMultiplier * (teamTechSpeed - 1)
+            team.rules().apply {
+                blockHealthMultiplier *= teamTechSpeed
+                blockDamageMultiplier *= teamTechSpeed
+                unitDamageMultiplier *= teamTechSpeed
 
-            team.team.rules().buildSpeedMultiplier *= teamTechSpeed
-            team.team.rules().unitBuildSpeedMultiplier *= teamTechSpeed
+                buildSpeedMultiplier *= teamTechSpeed
+                unitBuildSpeedMultiplier *= teamTechSpeed
+            }
 
-            "[#${team.team.color}]${team.team.name}[white]队科技属性提升：[red]${team.team.rules().blockHealthMultiplier / initState[team.team]!!}"
+            val rate = team.rules().blockHealthMultiplier
+            "[#${team.color}]${team.name}[white]队科技属性提升：[sky]${"%.3f".format(rate)} " +
+                    "${if (delta >= 0) "[green]+" else "[red]"}${"%.4f".format(delta)}"
         }
         Call.infoPopup(
-            infotext, 2.013f,
+            infotext, 5.013f,
             Align.topLeft, 350, 0, 0, 0
         )
     }
