61b3628c65510a9a51b7a92de3f764e915793430
way-zer <himc.wicp@gmail.com>
2022-04-23 23:45:22 +0800

    :sparkles: (wayzer/ext/autoUpdate) 更新重写自动更新服务端功能

--- scripts/wayzer/ext/autoUpdate.kts
+++ scripts/wayzer/ext/autoUpdate.kts
@@ -1,6 +1,6 @@
 package wayzer.ext
 
-import arc.util.Http
+import arc.util.Interval
 import arc.util.Log
 import arc.util.async.Threads
 import arc.util.serialization.Jval
@@ -9,9 +9,6 @@ import mindustry.gen.Groups
 import mindustry.net.BeControl
 import java.io.File
 import java.net.URL
-import kotlin.coroutines.resume
-import kotlin.coroutines.resumeWithException
-import kotlin.coroutines.suspendCoroutine
 import kotlin.system.exitProcess
 
 name = "自动更新"
@@ -21,19 +18,24 @@ val source by config.key("Anuken/Mindustry", "服务端来源，Github仓库")
 
 var updateCallback: (() -> Unit)? = null
 
-suspend fun http(url: String) = suspendCoroutine<Http.HttpResponse> {
-    Http.get(url, it::resume, it::resumeWithException)
-}
-
-suspend fun download(url: String, file: File): Long = withContext(Dispatchers.IO) {
-    val con = URL(url).openConnection()
-    val steam = con.getInputStream()
-    coroutineContext.job.invokeOnCompletion { steam.close() }
-    return@withContext steam.use { input ->
+suspend fun download(url: String, file: File): Int = withContext(Dispatchers.IO) {
+    val steam = URL(url).openStream()
+    val buffer = ByteArray(128 * 1024)//128KB
+    val logInterval = Interval()
+    var len = 0
+    steam.use { input ->
         file.outputStream().use { output ->
-            input.copyTo(output)
+            while (isActive) {
+                val i = input.read(buffer)
+                if (i == -1) break
+                output.write(buffer, 0, i)
+                len += i
+                if (logInterval[60f])
+                    logger.info("Downloaded ${len / 1024}KB")
+            }
         }
     }
+    return@withContext len
 }
 
 onEnable {
@@ -41,22 +43,22 @@ onEnable {
         while (true) {
             if (enableUpdate) {
                 try {
-                    val res = http("https://api.github.com/repos/$source/releases")
-                    if (res.status == Http.HttpStatus.OK) {
-                        val json = Jval.read(res.resultAsString).asArray().first()
-                        val newBuild = json.getString("tag_name", "")
-                        val (version, revision) = ("$newBuild.0").split(".").map { it.toInt() }
-                        if (version > Version.build || revision > Version.revision) {
-                            val asset = json.get("assets").asArray().find {
-                                it.getString("name", "").startsWith("server-release")
-                            }
-                            val url = asset.getString("browser_download_url", "")
-                            try {
-                                update(newBuild, "https://gh.api.99988866.xyz/$url")
-                                break
-                            } catch (e: Throwable) {
-                                logger.warning("下载更新失败: " + e.message)
-                            }
+                    val txt = URL("https://api.github.com/repos/$source/releases").readText()
+                    val json = Jval.read(txt).asArray().first()
+                    val newBuild = json.getString("tag_name", "")
+                    val (version, revision) = ("$newBuild.0").removePrefix("v")
+                        .split(".").map { it.toInt() }
+                    if (version > Version.build || revision > Version.revision) {
+                        val asset = json.get("assets").asArray().find {
+                            it.getString("name", "").startsWith("server-release")
+                        }
+                        val url = asset.getString("browser_download_url", "")
+                        try {
+                            update(newBuild, "https://gh.tinylake.cf/$url")
+                            break
+                        } catch (e: Throwable) {
+                            logger.warning("下载更新失败: " + e.message)
+                            e.printStackTrace()
                         }
                     }
                 } catch (e: Throwable) {
@@ -78,20 +80,22 @@ suspend fun update(version: String, url: String) {
         tmp.delete()
         throw e
     }
-    Log.info("新版本 $version 下载完成: ${size / 1024 / 1024}MB")
+    Log.info("新版本 $version 下载完成: ${size / 1024}KB")
     updateCallback = {
         Groups.player.forEach {
             it.kick("[yellow]服务器重启更新到新版本 $version")
         }
         Threads.sleep(32L)
-        Log.info("&lcVersion downloaded, exiting. Note that if you are not using a auto-restart script, the server will not restart automatically.")
-        tmp.copyTo(dest, true)
+        dest.outputStream().use { output ->
+            tmp.inputStream().use { it.copyTo(output) }
+        }
         tmp.delete()
+        Log.info("&lcVersion downloaded, exiting. Note that if you are not using a auto-restart script, the server will not restart automatically.")
         exitProcess(2)
     }
     broadcast("[yellow]服务器新版本{version}下载完成,将在本局游戏后自动重启更新".with("version" to version))
 }
 
-listen<EventType.WorldLoadEvent> {
+listen<EventType.ResetEvent> {
     updateCallback?.invoke()
 }
