3df2f6861c3c499c4b221d6d2d90e5b07002f226
way-zer <himc.wicp@gmail.com>
2022-05-22 19:50:06 +0800

    :wrench: (wayzer/ext/resourceHelper) 移除数据上报功能 (已废弃)

--- scripts/wayzer/ext/resourceHelper.kts
+++ scripts/wayzer/ext/resourceHelper.kts
@@ -4,20 +4,17 @@ package wayzer.ext
 
 import arc.files.Fi
 import arc.struct.StringMap
-import arc.util.Http
 import arc.util.serialization.JsonReader
 import arc.util.serialization.JsonValue
 import arc.util.serialization.JsonWriter
 import cf.wayzer.placehold.PlaceHoldApi.with
 import mindustry.game.Gamemode
-import mindustry.gen.Groups
 import wayzer.MapInfo
 import wayzer.MapProvider
 import wayzer.MapRegistry
 import java.io.ByteArrayInputStream
 import java.net.HttpURLConnection
 import java.net.URL
-import kotlin.math.max
 import mindustry.maps.Map as MdtMap
 
 name = "资源站配套脚本"
@@ -83,116 +80,3 @@ MapRegistry.register(this, object : MapProvider() {
         }
     }
 })
-
-fun <K, V> Map<K, V>.toJson(body: (V) -> String) = entries.joinToString(",", "{", "}") {
-    "\"${it.key}\":${body(it.value)}"
-}
-
-//数据上报
-var highestWave = 1
-fun postRecord(mapId: String, type: String, data: String) {
-    if (!tokenOk) return
-    Http.post("$webRoot/api/maps/$mapId/record?token=$token&event=$type", data)
-        .header("content-type", "application/json")
-        .submit { }
-}
-
-listen<EventType.PlayEvent> {
-    val map = state.map
-    val id = map.resourceId ?: return@listen
-    postRecord(id, "Start", mapOf("wave" to state.wave).toJson { it.toString() })
-}
-onEnable {
-    launch {
-        while (true) {
-            delay(300_000)
-            val id = state.map?.resourceId ?: continue
-            highestWave = state.wave
-            postRecord(id, "Gaming", mapOf("wave" to highestWave).toJson { it.toString() })
-        }
-    }
-}
-
-//评分模块
-val rateLimit = 30 * 60 //约30分钟的贡献
-val stats = mutableMapOf<Long, Int>()
-
-@Savable(false)
-val rate = mutableMapOf<Long, Int>()
-customLoad(::rate) {
-    rate.putAll(it)
-}
-fun tryUpdateStats(p: Player) {
-    val profile = PlayerData[p.uuid()].secureProfile(p)?.qq ?: return
-    val score = "{p.statistics.score}".with("p" to p).toString().toFloatOrNull()?.toInt() ?: return
-    val old = stats[profile] ?: 0
-    stats[profile] = score
-    arrayOf(30, 35, 40, 45, 50, 55, 60).find { it * 60 in (old + 1)..score }?.let { time ->
-        if (profile in rate) return
-        launch(Dispatchers.game) {
-            p.sendMessage("[yellow]你已经游玩该图达到${time}分钟，有何感想? 输入/rate [red]1-10的整数[]评个分吧")
-        }
-    }
-}
-
-onEnable {
-    launch {
-        while (true) {
-            delay(30_000)
-            if (tokenOk && state.map.resourceId != null) {
-                Groups.player.forEach(::tryUpdateStats)
-            }
-        }
-    }
-}
-var hasGameOver = false
-fun postEnd() {
-    val id = state.map.resourceId ?: return
-    val data = mapOf("wave" to highestWave, "stats" to stats.filterValues { it > 15_000 }, "rate" to rate)
-        .toJson { map ->
-            if (map is Map<*, *>) map.toJson { it.toString() }
-            else map.toString()
-        }
-    if (rate.isNotEmpty()) {
-        val copy = rate.values.toList()
-        launch(Dispatchers.game) {
-            broadcast("[green]共有{num}人评分,平均分为{rate:%.1f}".with("num" to copy.size, "rate" to copy.average()))
-        }
-    }
-    stats.clear()
-    rate.clear()
-    postRecord(id, "End", data)
-}
-listen<EventType.GameOverEvent> {
-    highestWave = max(highestWave, state.wave)
-    Groups.player.forEach(::tryUpdateStats)
-    postEnd()
-    hasGameOver = true
-}
-
-listen<EventType.ResetEvent> {
-    if (!hasGameOver)
-        postEnd()
-    hasGameOver = false
-}
-
-command("rate", "对地图进行评分") {
-    type = CommandType.Client
-    usage = "<1-10的整数>"
-    aliases = listOf("评分")
-    permission = "wayzer.rate"
-    body {
-        if (!tokenOk || state.map.resourceId == null)
-            returnReply("[red]评分未开放(需要通过/vote web换图才能评分哦)".with())
-        if (hasGameOver)
-            returnReply("[yellow]评分已结束".with())
-        val score = arg.getOrNull(0)?.toIntOrNull() ?: replyUsage()
-        if (score !in 1..10) replyUsage()
-        val profile = PlayerData[player!!.uuid()].secureProfile(player!!)?.qq
-            ?: returnReply("[red]仅绑定用户可以评分".with())
-        if (stats[profile] ?: 0 < rateLimit)
-            returnReply("[red]游玩该地图30分钟后再来评分".with())
-        rate[profile] = score
-        reply("[green]评分成功(修改请重新评分)".with())
-    }
-}
