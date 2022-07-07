fd85838f2e7f90b8ac41319cee52a3808eaf7ebe
way-zer <himc.wicp@gmail.com>
2022-07-07 16:31:22 +0800

    :sparkles: (wayzer/map/banUnit) 排除负载源和地图预先布置的单位

--- scripts/wayzer/map/banUnit.kts
+++ scripts/wayzer/map/banUnit.kts
@@ -2,6 +2,8 @@ package wayzer.map
 
 import mindustry.ctype.ContentType
 import mindustry.type.UnitType
+import mindustry.world.blocks.units.Reconstructor
+import kotlin.random.Random
 
 var list = emptySet<UnitType>()
 
@@ -22,9 +24,16 @@ listen<EventType.PlayerJoin> { e ->
         e.player.sendMessage("[red]本地图禁用单位:[yellow]{list}".with("list" to list.map { "${it.emoji()}${it.localizedName}" }))
 }
 
+val specialFlag = Random.nextDouble()
+
+listen<EventType.UnitCreateEvent> {
+    if (it.unit.type in list && it.spawner is Reconstructor.ReconstructorBuild)
+        it.unit.flag = specialFlag
+}
+
 listen<EventType.UnitUnloadEvent> {
     val type = it.unit.type
-    if (type in list) {
+    if (type in list && it.unit.flag == specialFlag) {
         Call.label("[red]本地图已禁用单位${type.emoji()}${type.localizedName}}", 10f, it.unit.x, it.unit.y)
         it.unit.set(0f, 0f)
         it.unit.kill()
