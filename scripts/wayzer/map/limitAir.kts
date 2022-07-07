914f03f3594201ad24c819092b0a1ed888794b44
way-zer <himc.wicp@gmail.com>
2022-07-07 16:32:38 +0800

    :bug: (wayzer/map/limitAir) 可能的异步读写问题

--- scripts/wayzer/map/limitAir.kts
+++ scripts/wayzer/map/limitAir.kts
@@ -8,15 +8,13 @@ import mindustry.gen.Groups
 val enable get() = state.rules.tags.getBool("@limitAir")
 
 onEnable {
-    launch {
-        while (true) {
-            delay(3_000)
-            if (net.server() && enable) {
-                Groups.unit.forEach {
-                    if (it.type().flying && state.teams.closestEnemyCore(it.x, it.y, it.team)?.within(it, state.rules.enemyCoreBuildRadius) == true) {
-                        it.player?.sendMessage("[red]该地图限制空军,禁止进入敌方领空".with())
-                        it.kill()
-                    }
+    loop(Dispatchers.game){
+        delay(3_000)
+        if (net.server() && enable) {
+            Groups.unit.forEach {
+                if (it.type().flying && state.teams.closestEnemyCore(it.x, it.y, it.team)?.within(it, state.rules.enemyCoreBuildRadius) == true) {
+                    it.player?.sendMessage("[red]该地图限制空军,禁止进入敌方领空".with())
+                    it.kill()
                 }
             }
         }
