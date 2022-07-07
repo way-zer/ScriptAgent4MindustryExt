1908d7cbfa5340281b2ac573d93fe7be2c299f3b
way-zer <himc.wicp@gmail.com>
2022-07-07 16:32:00 +0800

    :sparkles: (wayzer/map/betterTeam) team指令支持显示更多队伍

--- scripts/wayzer/map/betterTeam.kts
+++ scripts/wayzer/map/betterTeam.kts
@@ -115,8 +115,7 @@ command("team", "管理指令: 修改自己或他人队伍(PVP模式)") {
     body {
         if (!state.rules.pvp) returnReply("[red]仅PVP模式可用".with())
         val team = arg.getOrNull(0)?.toIntOrNull()?.let { Team.get(it) } ?: let {
-            val teams = Team.baseTeams
-                .mapIndexed { i, t -> "{id}({team.colorizeName}[])".with("id" to i, "team" to t) }
+            val teams = allTeam.map { t -> "{id}({team.colorizeName}[])".with("id" to t.id, "team" to t) }
             returnReply("[yellow]可用队伍: []{list}".with("list" to teams))
         }
         val player = arg.getOrNull(1)?.let {
