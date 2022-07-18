fc448f43d016d065f4ed76fc1db142eebf46eba8
way-zer <himc.wicp@gmail.com>
2022-07-18 13:56:55 +0800

    :arrow_up: (loader) 游戏版本到136

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "cf.wayzer"
-version = "v1.x.x" //采用3位版本号v1.2.3 1为大版本 2为插件版本 3为脚本版本
+version = "v3.x.x" //采用3位版本号v1.2.3 1为大版本 2为插件版本 3为脚本版本
 
 if (File(".git").isDirectory)
     gitVersioning.apply(closureOf<me.qoomon.gradle.gitversioning.GitVersioningPluginConfig> {
