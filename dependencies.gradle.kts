8114e1cda9d7794d4e9a161b02b05bdceed224ee
way-zer <himc.wicp@gmail.com>
2022-08-12 14:55:19 +0800

    :heavy_plus_sign: (core/contentsTweaker.kts) ContentsLoader 2.0

--- dependencies.gradle.kts
+++ dependencies.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
 //    compile("org.jline:jline-terminal-jansi:3.19.0")
     implementation("org.jline:jline-reader:3.19.0")
     //coreMindustry/utilContentsOverwrite
-    implementation("cf.wayzer.MindustryContents:contents:1.0.8")
+    implementation("cf.wayzer:ContentsTweaker:v2.0.1")
 
     //mirai
     implementation("net.mamoe:mirai-core-api-jvm:2.10.0")
