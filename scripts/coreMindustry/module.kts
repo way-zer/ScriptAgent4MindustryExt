eb57c71d260e7ebae6bc67bedd01de1c8c2e5c1b
way-zer <himc.wicp@gmail.com>
2022-10-24 11:26:45 +0800

    :sparkles: (core) 更多default import

--- scripts/coreMindustry/module.kts
+++ scripts/coreMindustry/module.kts
@@ -2,10 +2,12 @@
 @file:Import("arc.Core", libraryByClass = true)
 @file:Import("mindustry.Vars", libraryByClass = true)
 @file:Import("arc.Core", defaultImport = true)
+@file:Import("mindustry.Vars.*", defaultImport = true)
+@file:Import("mindustry.content.*", defaultImport = true)
 @file:Import("mindustry.gen.Player", defaultImport = true)
 @file:Import("mindustry.gen.Call", defaultImport = true)
+@file:Import("mindustry.gen.Groups", defaultImport = true)
 @file:Import("mindustry.game.EventType", defaultImport = true)
-@file:Import("mindustry.Vars.*", defaultImport = true)
 @file:Import("coreMindustry.lib.*", defaultImport = true)
 
 package coreMindustry
