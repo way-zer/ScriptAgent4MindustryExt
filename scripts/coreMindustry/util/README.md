106d7675a3a21bbcabb8666fd2987285e931c23c
Cong0707 <49234756+Cong0707@users.noreply.github.com>
2022-11-11 20:00:05 +0800

    Delete scripts directory

--- scripts/coreMindustry/util/README.md
+++
@@ -1,3 +0,0 @@
-## 扩展工具库
-存储一些常用工具函数，但不是必需功能
-请通过`@file:Import("@coreMindustry/util/trackBuilding.kt", sourceFile = true)`导入
--- scripts/mirai/simple.kts
+++
@@ -1,9 +0,0 @@
-package mirai
-
-globalEventChannel().subscribeGroupMessages {
-    contains("hello", true).reply {
-        QuoteReply(message) + "你好" + At(sender)
-    }
-    content { message.filterIsInstance<PlainText>().any { it.content.contains("欢迎新人") } }
-        .reply { message.firstIsInstanceOrNull<At>()?.let { PlainText("欢迎") + it } ?: "欢迎新人!" }
-}
