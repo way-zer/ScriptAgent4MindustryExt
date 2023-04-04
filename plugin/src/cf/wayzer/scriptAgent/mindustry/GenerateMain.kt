77d52a666772536a2ca8cdaa5c61f059f6d182a9
way-zer <himc.wicp@gmail.com>
2023-04-04 23:33:36 +0800

    :arrow_up: (loader) 核心库v1.10.3; Kotlin v1.8.20

--- plugin/src/cf/wayzer/scriptAgent/mindustry/GenerateMain.kt
+++ plugin/src/cf/wayzer/scriptAgent/mindustry/GenerateMain.kt
@@ -32,7 +32,7 @@ object GenerateMain {
     fun afterLoad(args: Array<String>) {
         DependencyManager {
             addRepository("https://www.jitpack.io/")
-            requireWithChildren(Dependency.parse("com.github.TinyLake.MindustryX:core:v140.101"))
+            requireWithChildren(Dependency.parse("com.github.TinyLake.MindustryX:core:v143.001"))
             loadToClassLoader(GenerateMain::class.java.classLoader)
         }
 
