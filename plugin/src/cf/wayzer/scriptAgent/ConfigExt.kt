e89ceed1cb9fcbacd604842f2aeabc1a45b44a36
way-zer <himc.wicp@gmail.com>
2023-04-04 23:30:39 +0800

    :sparkles: (loader) 多重mainScript支持

--- plugin/src/cf/wayzer/scriptAgent/ConfigExt.kt
+++ plugin/src/cf/wayzer/scriptAgent/ConfigExt.kt
@@ -5,10 +5,32 @@ package cf.wayzer.scriptAgent
 import arc.util.CommandHandler
 import cf.wayzer.scriptAgent.util.DSLBuilder
 
+//Experimental
+object MainScriptsHelper {
+    const val defaultMain = "main/bootStrap"
+    var list: List<String> = emptyList()
+        private set
+    private var cur = 0
+    val current get() = list.getOrElse(cur) { defaultMain }
+
+    internal fun load() {
+        val params = System.getenv("SAMain")
+        Config.logger.info("SAMain=${params ?: defaultMain}")
+        if (params != null)
+            list = params.split(";")
+    }
+
+    fun next(): String {
+        if (cur < list.size) cur++
+        else error("Already default main.")
+        return current
+    }
+}
+
 var Config.version by DSLBuilder.lateInit<String>()
     internal set
-var Config.mainScript by DSLBuilder.lateInit<String>()
-    internal set
+val Config.mainScript get() = MainScriptsHelper.current
+fun Config.nextMainScript() = MainScriptsHelper.next()
 var Config.clientCommands by DSLBuilder.lateInit<CommandHandler>()
     internal set
 var Config.serverCommands by DSLBuilder.lateInit<CommandHandler>()
--- plugin/src/cf/wayzer/scriptAgent/mindustry/Main.kt
+++ plugin/src/cf/wayzer/scriptAgent/mindustry/Main.kt
@@ -6,6 +6,7 @@ import arc.files.Fi
 import arc.util.CommandHandler
 import arc.util.Log
 import cf.wayzer.scriptAgent.*
+import cf.wayzer.scriptAgent.MainScriptsHelper.defaultMain
 import cf.wayzer.scriptAgent.define.LoaderApi
 import kotlinx.coroutines.runBlocking
 import mindustry.Vars
@@ -17,7 +18,7 @@ class Main(private val loader: Plugin) : Plugin() {
     override fun getConfig(): Fi = loader.config
 
     override fun registerClientCommands(handler: CommandHandler) {
-        //after init() is too late
+        //call after init(), too late
 //        Config.clientCommands = handler
     }
 
@@ -26,13 +27,8 @@ class Main(private val loader: Plugin) : Plugin() {
     }
 
     override fun init() {
-        val defaultMain = "main/bootStrap"
-        val version = Vars.mods.getMod(loader.javaClass).meta.version
-        val main = System.getenv("SAMain") ?: defaultMain
-        Log.info("SAMain=$main")
-
-        Config.version = version
-        Config.mainScript = main
+        MainScriptsHelper.load()
+        Config.version = Vars.mods.getMod(loader.javaClass).meta.version
         Config.rootDir = Vars.dataDirectory.child("scripts").file()
         Config.clientCommands = Vars.netServer?.clientCommands ?: CommandHandler("/")
         if (!Vars.headless) {
@@ -43,7 +39,7 @@ class Main(private val loader: Plugin) : Plugin() {
         tryExtract("/res/$defaultMain.ktc", Config.cacheFile(defaultMain, false))
         ScriptRegistry.scanRoot()
 
-        val script = ScriptRegistry.findScriptInfo(main)
+        val script = ScriptRegistry.findScriptInfo(Config.mainScript)
         if (script != null) runBlocking {
             ScriptManager.transaction {
                 add(script)
@@ -64,12 +60,12 @@ class Main(private val loader: Plugin) : Plugin() {
         }
 
         Log.info("&y===========================")
-        Log.info("&lm&fb     ScriptAgent &b$version         ")
+        Log.info("&lm&fb     ScriptAgent &b${Config.version}")
         Log.info("&b           By &cWayZer    ")
         Log.info("&b插件官网: https://git.io/SA4Mindustry")
         Log.info("&bQQ交流群: 1033116078")
         if (script == null)
-            Log.warn("&c未找到启动脚本(SAMain=$main),请下载安装脚本包,以发挥本插件功能")
+            Log.warn("&c未找到启动脚本(${Config.mainScript}),请下载安装脚本包,以发挥本插件功能")
         else {
             val all = ScriptRegistry.allScripts { true }
             Log.info(
