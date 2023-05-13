a4e36b1c22897fb325b77982c0cf5b63629feb3f
way-zer <himc.wicp@gmail.com>
2023-05-13 11:01:06 +0800

    :heavy_plus_sign: (core/utilTextInput) textInput封装

---
+++ scripts/coreMindustry/utilTextInput.kts
@@ -0,0 +1,30 @@
+package coreMindustry
+
+import coreLibrary.lib.util.nextEvent
+import mindustry.game.EventType.TextInputEvent
+import kotlin.random.Random
+
+data class OnTextInputResult(val player: Player, val id: Int, val text: String?) : Event, Event.Cancellable {
+    override var cancelled: Boolean = false
+
+    companion object : Event.Handler()
+}
+
+listen<TextInputEvent> {
+    OnTextInputResult(it.player, it.textInputId, it.text).launchEmit()
+}
+
+suspend fun textInput(
+    player: Player,
+    title: String,
+    message: String = "",
+    default: String = "",
+    lengthLimit: Int = Int.MAX_VALUE,
+    isNumeric: Boolean = false,
+    timeoutMillis: Int = 60_000
+): String? = withTimeoutOrNull(timeoutMillis.toLong()) {
+    val id = Random.nextInt(Int.MIN_VALUE, 0)
+    Call.textInput(player.con, id, title, message, lengthLimit, default, isNumeric)
+    nextEvent<OnTextInputResult> { it.player == player && it.id == id }.text
+}
+export(::textInput)
