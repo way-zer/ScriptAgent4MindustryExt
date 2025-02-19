2dc6bc491d7ee51dd756d5c5d9a1a17d5c31774d
way-zer <himc.wicp@gmail.com>
2025-02-19 13:57:58 +0800

    ✨(build) 支持在scripts下面挂载子项目(@命名空间)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,3 +25,10 @@ dependencyResolutionManagement {
 }
 
 include("scripts")
+
+file("scripts").listFiles()?.forEach {
+    if (it.isDirectory && it.name.startsWith("@")) {
+        include("scripts:${it.name.substring(1)}")
+        project(":scripts:${it.name.substring(1)}").projectDir = it
+    }
+}
