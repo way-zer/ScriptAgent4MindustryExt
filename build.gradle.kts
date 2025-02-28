6302c977dcdb1a92d7e2f0db5af576a872cf4866
way-zer <himc.wicp@gmail.com>
2025-02-28 22:59:07 +0800

    ⬆️(loader) library v1.11.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ sourceSets {
 
 
 dependencies {
-    val libraryVersion = "1.11.1.3"
+    val libraryVersion = "1.11.2.1"
     val mindustryVersion = "ca40f700fb" //v146.004
     api("cf.wayzer:ScriptAgent:$libraryVersion")
     implementation("cf.wayzer:LibraryManager:1.6")
