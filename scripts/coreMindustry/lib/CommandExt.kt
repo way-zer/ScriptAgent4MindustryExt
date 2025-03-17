3d583425053b517e571c1a5f0f8fa568ee021c7f
way-zer <himc.wicp@gmail.com>
2025-03-17 13:53:36 +0800

    🚑️(coreMindustry) 移除`Script.player`, 干扰`CommandContext.player`解析

--- scripts/coreMindustry/lib/CommandExt.kt
+++ scripts/coreMindustry/lib/CommandExt.kt
@@ -6,7 +6,6 @@ import coreLibrary.lib.CommandHandler
 import coreLibrary.lib.CommandInfo
 import coreLibrary.lib.command
 import coreLibrary.lib.with
-import mindustry.gen.Player
 
 /**
  * 注册指令
@@ -30,13 +29,3 @@ fun Script.command(name: String, description: String, init: CommandInfo.() -> Un
         body(handler)
     }
 }
-
-//常见拼写错误，但不报错
-@Suppress("unused")
-@Deprecated(
-    "请检查变量是否使用正确, Vars.player 为null",
-    ReplaceWith("error(\"服务器中不允许使用该变量\")"),
-    DeprecationLevel.ERROR
-)
-val Script.player: Player
-    get() = error("服务器中不允许使用该变量")
