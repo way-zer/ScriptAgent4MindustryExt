a3076b8aa5e35f170f4baed4274a6d8dca91bd7e
way-zer <himc.wicp@gmail.com>
2025-04-06 14:59:17 +0800

    👽️(wayzer/reGrief/limitFire) 使用新API获取火焰个数

--- scripts/wayzer/reGrief/limitFire.kts
+++ scripts/wayzer/reGrief/limitFire.kts
@@ -1,23 +1,10 @@
 package wayzer.reGrief
 
-import arc.struct.IntMap
-import mindustry.entities.Fires
-
 val limit by config.key(500, "火焰格数限制")
 
-val fireMap by lazy {
-    Fires::class.java.getDeclaredField("map").run {
-        isAccessible = true
-        get(null) as IntMap<*>
-    }
-}
-val fireCount get() = fireMap.size
-
+val fireCount get() = Groups.fire.size()
 var done = false
 listen<EventType.ResetEvent> { done = false }
-
-onEnable { fireMap }
-
 listen(EventType.Trigger.update) {
     if (state.rules.fire && fireCount > limit) {
         done = true
