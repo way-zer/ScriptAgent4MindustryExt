683574f5c801045a484c9fd54f4f49ff515ed5bf
way-zer <himc.wicp@gmail.com>
2025-06-23 23:35:22 +0800

    ⬆️ MDTX v2025.06.X10

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ sourceSets {
 
 dependencies {
     val libraryVersion = "1.11.3.1"
-    val mindustryVersion = "v2025.05.X9" //v149
+    val mindustryVersion = "v2025.06.X10" //v149
     api("cf.wayzer:ScriptAgent:$libraryVersion")
     implementation("cf.wayzer:LibraryManager:1.6")
 
--- scripts/bootStrap/generate.kts
+++ scripts/bootStrap/generate.kts
@@ -21,7 +21,7 @@ onEnable {
         return@onEnable ScriptManager.disableScript(this, "仅可通过SAMAIN启用")
     DependencyManager {
         addRepository("https://www.jitpack.io/")
-        require(Dependency.parse("com.github.TinyLake.MindustryX:core:v2025.05.X9"))
+        require(Dependency.parse("com.github.TinyLake.MindustryX:core:v2025.06.X10"))
         loadToClassLoader(Config.mainClassloader)
     }
     ScriptManager.transaction {
