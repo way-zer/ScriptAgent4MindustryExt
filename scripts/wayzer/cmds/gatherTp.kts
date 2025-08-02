50b5f371fa5e66ff7173b7b35fa77e1c81b681c1
way-zer <himc.wicp@gmail.com>
2025-08-02 15:21:55 +0800

    ✨(wayzer/cmd/gatherTp) 作为特殊技能实现

--- scripts/wayzer/cmds/gatherTp.kts
+++ scripts/wayzer/cmds/gatherTp.kts
@@ -1,48 +1,48 @@
+@file:Depends("wayzer/user/ext/skills", "Gather也算技能")
+
 package wayzer.cmds
 
-import arc.math.geom.Vec2
-import mindustry.core.World
+import cf.wayzer.placehold.PlaceHoldApi.with
 import mindustry.entities.Units
 import mindustry.gen.Unit
+import mindustry.world.Tile
+import wayzer.user.ext.SkillCooldown
+import wayzer.user.ext.SkillNoPvp
+import wayzer.user.ext.SkillPrecheck
+import wayzer.user.ext.skillBody
 import java.time.Duration
 import java.time.Instant
 
 PermissionApi.registerDefault("wayzer.ext.gather")
 
-var lastPos: Vec2 = Vec2.ZERO
+var lastPos: Tile? = null
 var lastTime: Instant = Instant.MIN
 
 command("gather", "发出集合请求") {
     usage = "[可选说明]"
-    type = CommandType.Client
     aliases = listOf("集合")
-    permission = "wayzer.ext.gather"
-    body {
-        if (state.rules.pvp)
-            returnReply("[red]PVP模式禁用".with())
-        if (player!!.dead() || !player!!.unit().type.targetable)
+    attr(SkillPrecheck)
+    attr(SkillNoPvp)
+    attr(SkillCooldown(30_000))
+    requirePermission("wayzer.ext.gather")
+    skillBody {
+        if (player.dead() || !player.unit().type.targetable)
             returnReply("[red]当前单位无法使用 集合".with())
-        if (Duration.between(lastTime, Instant.now()) < Duration.ofSeconds(30)) {
-            returnReply("[red]刚刚有人发起请求,请稍等30s再试".with())
+        if (Duration.between(lastTime, Instant.now()) < Duration.ofSeconds(10)) {
+            returnReply("[red]刚刚有人发起请求,请稍等10s再试".with())
         }
         val message = "[white]\"${arg.firstOrNull() ?: ""}[white]\""
-        lastPos = player!!.unit().run { Vec2(lastX, lastY) }
+        val tile = player.tileOn() ?: returnReply("[red]请在地图内使用".with())
+        lastPos = tile
         lastTime = Instant.now()
-        broadcast(
-            "[yellow][集合][cyan]{player.name}[white]发起集合([red]{x},{y}[white]){message},输入\"[gold]go[white]\"前往"
-                .with(
-                    "player" to player!!,
-                    "x" to lastPos.x.toInt() / 8,
-                    "y" to lastPos.y.toInt() / 8,
-                    "message" to message
-                ), quite = true
-        )
+        broadcastSkill("集合(${tile.x},${tile.y})")
+        broadcast("可输入\"[gold]go[white]\"前往：{message}".with("message" to message), quite = true)
     }
 }
 
 command("tp", "传送到鼠标坐标") {
-    type = CommandType.Client
-    permission = "wayzer.ext.tp"
+    attr(ClientOnly)
+    requirePermission("wayzer.ext.tp")
     body {
         val player = player!!
         player.unit()?.apply {
@@ -52,30 +52,25 @@ command("tp", "传送到鼠标坐标") {
     }
 }
 
-fun check(unit: Unit, x: Float, y: Float): Boolean {
-    unit.solidity()?.let {
-        if (it.solid(World.toTile(x), World.toTile(y)))
-            return false
-    }
-    if (!unit.type.flying && Units.count(x, y, unit.physicSize()) {
-            it.isGrounded && it.type.allowLegStep == unit.type.allowLegStep
-        } > 0)
-        return false
-    return true
+fun check(unit: Unit, tile: Tile): Boolean {
+    if (unit.type.flying) return true
+    return unit.canPass(tile.x.toInt(), tile.y.toInt()) &&
+            Units.count(tile.worldx(), tile.worldy(), unit.physicSize()) { it.isGrounded && it.hitSize > 14.0F } > 0
 }
 listen<EventType.PlayerChatEvent> {
-    if (it.message.equals("go", true) && lastPos != Vec2.ZERO) {
+    val tile = lastPos ?: return@listen
+    if (it.message.equals("go", true)) {
         it.player.unit()?.apply {
-            if (!check(this, lastPos.x, lastPos.y)) {
+            if (!check(this, tile)) {
                 it.player.sendMessage("[yellow]目标位置无法安全传送")
                 return@listen
             }
-            set(lastPos)
+            set(tile)
             snapInterpolation()
         }
     }
 }
 
 listen<EventType.ResetEvent> {
-    lastPos = Vec2.ZERO
+    lastPos = null
 }
