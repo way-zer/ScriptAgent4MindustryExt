6a4988e71eaccd61a21489a077a6b30b7456ab2c
way-zer <himc.wicp@gmail.com>
2025-10-19 20:12:17 +0800

    ✨(wayzer/reGrief/autoChangeMap)

---
+++ scripts/wayzer/reGrief/autoChangeMap.kts
@@ -0,0 +1,35 @@
+@file:Depends("wayzer/maps", "换图")
+
+package wayzer.reGrief
+
+import wayzer.MapManager
+import kotlin.time.Duration.Companion.seconds
+
+//预防一些卡服图，玩家无法复活发起投票
+
+var newMap = false
+var counter = 0
+onEnable {
+    loop(Dispatchers.game) {
+        delay(1.seconds)
+        if (newMap || Groups.player.count { !it.dead() && it.unit().health > 0 } > 0) {
+            counter = 0
+            return@loop
+        }
+        counter += 1
+        if (counter > 100) {
+            broadcast("[red]无人游玩，5秒后自动换图".with())
+            delay(5.seconds)
+            MapManager.loadMap()
+        }
+    }
+}
+
+listen<EventType.ResetEvent> {
+    newMap = true
+}
+
+listen<EventType.ConnectPacketEvent> {
+    //Someone request connect, maybe want to play
+    newMap = false
+}
