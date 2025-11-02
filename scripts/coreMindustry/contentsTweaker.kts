3549eaa7d6940bbb13d882ceef4918889d2a5eb8
cong <49234756+Cong0707@users.noreply.github.com>
2025-11-02 22:11:15 +0800

    Update contentsTweaker.kts

--- scripts/coreMindustry/contentsTweaker.kts
+++ scripts/coreMindustry/contentsTweaker.kts
@@ -1,15 +1,12 @@
 @file:Import("https://www.jitpack.io/", mavenRepository = true)
 @file:Import("com.github.way-zer:ContentsTweaker:v3.1.2", mavenDependsSingle = true)
+@file:Depends("coreMindustry/menu", "调用菜单")
 
 package coreMindustry
 
 import arc.struct.Seq
 import arc.util.serialization.Jval
-
-registerVar("scoreboard.ext.contents-0-Version", "ContentPatcher状态显示", DynamicVar {
-    if (patches == null) return@DynamicVar null
-    "{cK}CP修改已加载: {cV}{count} 修改".with("count" to state.patcher.patches.size)
-})
+import mindustry.mod.ContentPatcher.PatchSet
 
 var patches: String?
     get() = state.map.tags.get("ContentsPatch")
@@ -65,5 +62,61 @@ listen<EventType.WorldLoadBeginEvent> {
         if (name.isBlank()) return@forEach
         val patch = state.map.tags.get("CT@$name") ?: return@forEach
         addPatch(name, patch)
+    }
+}
+
+command("cp", "查看cp修改") {
+    type = CommandType.Client
+    body {
+        menu(player!!)
+    }
+}
+
+suspend fun menu(player: Player, select: Int = 1) {
+    object : PagedMenuBuilder<PatchSet>(state.patcher.patches.toList(), selectedPage = select, 6) {
+        override suspend fun renderItem(item: PatchSet) {
+            option(if (item.name.isEmpty()) "<unnamed>" else item.name) {
+                moreMenu(player, selectedPage, item)
+            }
+        }
+
+        override suspend fun build() {
+            title = "[yellow]ContentPatcher"
+            msg = """
+            [sky]查询服务器patch列表
+            """.trimIndent()
+            super.build()
+        }
+    }.sendTo(player, 20_000)
+}
+
+suspend fun moreMenu(player: Player, select: Int, item: PatchSet) {
+    MenuBuilder(true){
+        title = "[yellow]Patch详情"
+        val jsonFlat = try {
+            val jval = Jval.read(item.patch)
+            flater(jval)
+        } catch (e: Exception) {
+            "[red]解析失败"
+        }
+        msg = """
+            [white]${jsonFlat}
+        """.trimIndent()
+        option ("[red]返回"){
+            menu(player, select)
+        }
+    }.sendTo(player, 20_000)
+}
+
+fun flater(jval: Jval, prefix: String = ""): String = buildString {
+    when {
+        jval.isObject -> jval.asObject().forEach {
+            val fullKey = if (prefix.isEmpty()) it.key else "$prefix.${it.key}"
+            append(flater(it.value, fullKey))
+        }
+        jval.isArray -> jval.asArray().forEachIndexed { index, value ->
+            append(flater(value, "$prefix[$index]"))
+        }
+        else -> append("$prefix = $jval\n")
     }
 }
