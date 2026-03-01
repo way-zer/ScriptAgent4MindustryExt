6225f75e1395badff6b236948871199decb8de56
way-zer <himc.wicp@gmail.com>
2026-03-01 21:37:59 +0800

    ⬆️(loader) v2.1.8.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,7 +79,7 @@ dependencies.constraints {
     api("dev.folia:folia-api:$bukkitVersion")
 }
 dependencies {
-    val libraryVersion = "2.1.8.4"
+    val libraryVersion = "2.1.8.5"
     api("cf.wayzer:ScriptAgent:${libraryVersion}")
     implementation("cf.wayzer:LibraryManager:1.6")
 
